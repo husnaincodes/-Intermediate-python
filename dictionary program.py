5263c7ec4c2b5d4f1af4042da368f9542164516f
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 19:51:25 +0500

    Dictionary Problem

---
+++ dictionary program.py
@@ -0,0 +1,14 @@
+
+words = {
+"madad" : "HELP",
+"mushkill" : "Difficulty",
+"billi" : "CAT",
+"janwar" : "ANIMALS"
+
+}
+word = input("PLEASE ENETR YOUR WORD :")
+# word1 = input("PLEASE ENETR YOUR WORD :")
+
+print(words[word])
+
+# print(words[word1])
