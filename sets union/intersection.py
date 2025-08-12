66625a0aef52eaa1c1ed3ed6e2db9ed6649cc10a
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 19:34:59 +0500

    UNION AND INTERSECTION

---
+++ sets union/intersection.py
@@ -0,0 +1,13 @@
+
+my_set = {2 ,4,6,8,10,12,14,15}
+
+my_set1 = {1.3,5,7,9,11,13,15}
+
+print(my_set.union(my_set1))
+
+print(my_set.intersection(my_set1))
+
+print(my_set-my_set1)
+
+print(my_set1-my_set)
+
