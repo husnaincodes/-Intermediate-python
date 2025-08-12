30e463108ebf50052c14c485d2d184444b11be1c
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 20:33:20 +0500

    CONDITIONS

---
+++ condition.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter your age please : "))
+
+
+if(age>=18):
+    print("You are valid for CNIC")
+else:
+    print("YOUR AGE IS INVALID FOR CNIC APPLICATION")
+print("THANK YOU")
---
+++ elif condtion.py
@@ -0,0 +1,15 @@
+
+age = int(input("Enter your age please : "))
+
+
+if(age>=18):
+    print("You are valid for CNIC")
+
+elif(age<=0):
+    print("INVALID DATA")
+
+
+
+else:
+    print("YOUR AGE IS INVALID FOR CNIC APPLICATION")
+print("THANK YOU")
