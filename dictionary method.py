7be1879b1ab6931eabc3290cbd91e8e9af3508fd
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 18:24:53 +0500

    DICTOINARY

---
+++ dictionary method.py
@@ -0,0 +1,27 @@
+
+marks ={
+    "HUSNAIN": 100,
+    "SHAHBAZ": 98,
+    "RAFAY"  : 88,
+    "AHMAD" :  89,
+    "list" : [1,4,5,6,7]
+   
+}
+print(marks,type(marks))
+
+print(marks.items())
+print(marks.keys())
+marks.update({"HUSNAIN": 87, "MAZZ":97})
+print(marks)
+print(marks.get("HUSNAIN"))
+print(marks["HUSNAIN"])
+
+
+print(marks.get("HUSNAIN2")) #none
+
+
+# The difference between these two is one give you an error if name is not in the marks (marks["HUSNIAn")
+# and other gives you NONE                                                                                        
+print(marks["HUSNAIN2"]) #error
+
+
---
+++ dictionary.py
@@ -0,0 +1,14 @@
+marks ={
+    "HUSNAIN": 100,
+    "SHAHBAZ": 98,
+    "RAFAY"  : 88,
+    "AHMAD" :  89,
+    "list" : [1,4,5,6,7]
+    # 0 : "LAHORE"
+}
+print(marks,type(marks))
+
+print(marks["AHMAD"])
+print(marks["HUSNAIN"])
+print(marks["SHAHBAZ"])
+print(marks["RAFAY"])
