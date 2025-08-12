d0d819653b5b9f61368094f4595c6fa86d6ff507
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 20:11:05 +0500

    DICTIONARY 2

---
+++ dict program2.py
@@ -0,0 +1,21 @@
+d = {}
+
+
+name = input("Enter your name please:")
+lang = input("Enter your language please :")
+d.update({name : lang})
+
+name = input("Enter your name please:")
+lang = input("Enter your language please :")
+d.update({name : lang})
+
+name = input("Enter your name please:")
+lang = input("Enter your language please :")
+d.update({name : lang})
+
+name = input("Enter your name please:")
+lang = input("Enter your language please :")
+d.update({name : lang})
+
+
+print(d)
