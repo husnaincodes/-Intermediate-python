f87668d3ac73b7c87c6e45dffd14d6837ff3de21
Husnain <Husnain.tayab@hotmail.com>
2025-08-12 21:04:40 +0500

    RESULT CARD

---
+++ result card.py
@@ -0,0 +1,36 @@
+
+number = int(input(" ENTER YOUR NUMBER PLEASE : "))
+
+Percentage = number/1100*100
+
+if(Percentage>=85):
+    print("Your Grade is A+ " )
+    print("Your percentage is :",Percentage)
+    print("Well done ")
+
+elif(Percentage>=75):
+    print("Your Grade is A ")
+    print("Your percentage is :",Percentage)
+    print("GOODKEEP IT UP")   
+
+elif(Percentage>=60):
+    print("Your Grade is B ")
+    print("Your percentage is :",Percentage)
+    print("keep it up")
+
+elif(Percentage>=45):
+    print("YOUR Grade is C")
+    print("Your percentage is :",Percentage)
+    print("Working Hard")
+
+elif(Percentage>=33):
+    print("YOUR ARE PASS ")
+    print("Your percentage is :",Percentage)
+
+else:
+    print("You are fail next time do your best ")
+    print("Your percentage is :",Percentage)
+
+print("Many congratulation who students pass and keep is up and do your best")
+
+
