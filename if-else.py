5a26c08abd00b06fb94780b1b836c6a6a915c7cf
Husnain <Husnain.tayab@hotmail.com>
2025-08-15 17:05:29 +0500

    IF-Else

---
+++ if-else.py
@@ -0,0 +1,34 @@
+
+
+list = [
+    "Ayaan",
+    "Zara",
+    "Hamza",
+    "Fatima",
+    "Ali",
+    "Hina",
+    "Bilal",
+    "Sara",
+    "Omar",
+    "Ayesha",
+    "Usman",
+    "Mariam",
+    "Hassan",
+    "Noor",
+    "Imran",
+    "Iqra",
+    "Shahzaib",
+    "Maha",
+    "Rizwan",
+    "Anaya",
+    "Husnain"
+]
+name = input("ENTER YOUR TO CHECK YOUR NAME IN THE LIST : ")
+
+if(name in list):
+    print("Your name in the list :", name )
+else:
+    print("Your name is not in the list ")
+
+
+print("Thank You!")
