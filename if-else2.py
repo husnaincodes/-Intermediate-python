bdeb9129cb232ae6302a4bee5bf245b7386f9e0e
Husnain <Husnain.tayab@hotmail.com>
2025-08-15 17:30:57 +0500

    If-else program

---
+++ if-else2.py
@@ -0,0 +1,22 @@
+topic = input("Enter your word or sentence : ")
+
+# topic = input("Enter your word or sentence :")
+
+# topic = input("Enter your word or sentence :")
+
+if("PHYiSCS".lower() in topic.lower()):
+   
+   print ("This topic or word from Phyiscs ")
+
+elif("Newton".lower() in topic.lower()):
+     
+     print ("This topic or word from Phyiscs ")
+
+elif( ("Laws of Motion").lower() in topic.lower() ):
+    
+
+       print ("This topic or word from Phyiscs :")
+elif("Gravity".lower() in topic):
+     print("This topic or word from Phyiscs ")
+else:
+     print("This is not phyiscs topic ")
