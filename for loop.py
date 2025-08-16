1ecd8a706ca1e6fea3505126ee62d184c1bd9939
Husnain <Husnain.tayab@hotmail.com>
2025-08-16 18:01:28 +0500

    For loop

---
+++ for loop.py
@@ -0,0 +1,27 @@
+
+list = [ 1 ,False, True,34.4,"code",4,19]
+for i in list :
+    print(i)
+
+
+
+
+
+
+
+tuple = (1 ,False, True,34.4,"code",4,19,"husnain")
+for i in tuple :
+    print(i)
+
+
+
+string = "husnaincodes"
+for i in string :
+    print(i)
+
+
+for i  in range(0,45):
+    print(i)
+
+
+
