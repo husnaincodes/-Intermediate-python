36a56693542e6bf7ad6c32650867cb9e4e0ba733
Husnain <Husnain.tayab@hotmail.com>
2025-08-18 20:35:13 +0500

    Program2

---
+++ program2.py
@@ -0,0 +1,19 @@
+
+number = int(input("Enter your number :"))
+
+
+for i in range (1,number+1):
+    if i == 1 or i == number :
+        print("*"*number,end="")
+    else:
+        print("*", end="")
+        print(" "*(number-2),end="")
+        print("*",end="")
+
+    print("")
+    
+
+    
+
+
+
