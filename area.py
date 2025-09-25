5f030e96f86693c0e700064074eab7a501f1471d
Husnain <Husnain.tayab@hotmail.com>
2025-09-25 19:54:37 +0500

    Area of Triangle

--- area.py
+++ area.py
@@ -1,8 +1,15 @@
 import math
+
 num1 = int(input("Enter the first side number: "))
+
 num2 = int(input("Enter the second side number: "))
+
 num3 = int(input("Enter the thrid side number: "))
+
 sum_sides =(num1+num2+num3)/2
-area = float(math.sqrt(sum_sides*(sum_sides-num1)*(sum_sides-num2)*(sum_sides-num3)))
-triangle_area = float(area)
-print(f" The area of Triangle is : {triangle_area}")  
+
+area = int(math.sqrt(sum_sides*(sum_sides-num1)*(sum_sides-num2)*(sum_sides-num3)))
+
+triangle_area = int(area)
+
+print(f"The area of Triangle is : {triangle_area}")  
