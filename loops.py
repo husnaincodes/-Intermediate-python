158e46a88001487f8cb8e2b08e7a90917e6e9b9c
Husnain <Husnain.tayab@hotmail.com>
2025-09-25 19:46:41 +0500

    Loop

--- loops.py
+++ loops.py
@@ -1,13 +1,13 @@
 
 number  = 1 
-while(number<=100):
+while(number<=1000):
     print(number)
     number+=1
 
 
 i = 0
 name = input("Enter your name please sir/madam : ")
-while(i<=5):
+while(i<=15):
     print(name)
     # i = i+1
     i = i+2
