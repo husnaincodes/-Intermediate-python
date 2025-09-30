feb2047815316b9a5ff78c4c2983c9647312e4e3
Husnain <Husnain.tayab@hotmail.com>
2025-09-30 15:15:21 +0500

    Dictionary Program

--- dict program2.py
+++ dict program2.py
@@ -2,18 +2,25 @@ dictionary = {}
 
 
 name = input("Enter your name please:")
+
 lang = input("Enter your language please :")
+
 dictionary.update({name : lang})
 
 name = input("Enter your name please:")
+
 lang = input("Enter your language please :")
 dictionary.update({name : lang})
 
+
 name = input("Enter your name please:")
+
 lang = input("Enter your language please :")
+
 dictionary.update({name : lang})
 
 name = input("Enter your name please:")
+
 lang = input("Enter your language please :")
 dictionary.update({name : lang})
 
