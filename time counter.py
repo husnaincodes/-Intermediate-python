4adb26068727baad7ff7e398692c32def92b99ad
Husnain <Husnain.tayab@hotmail.com>
2025-10-15 22:54:14 +0500

    Time counter

---
+++ time counter.py
@@ -0,0 +1,10 @@
+
+import time
+
+t = int(input("Enter seconds: "))
+while t:
+    mins, secs = divmod(t, 60)
+    print(f"{mins:02d}:{secs:02d}", end="\r")
+    time.sleep(1)
+    t -= 1
+print("⏰ Time’s up!")
