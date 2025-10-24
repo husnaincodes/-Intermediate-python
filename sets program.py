3458896f5e9d352f3c2516148b64c8d9a764ff46
Husnain <Husnain.tayab@hotmail.com>
2025-10-24 21:29:12 +0500

    input sets

--- sets program.py
+++ sets program.py
@@ -2,31 +2,24 @@
 my_set = set()
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 number = input("Enter your number : ")
-
 my_set.add(int(number))
 
 print(my_set)
