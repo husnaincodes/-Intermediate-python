ed8ad41a8e45134db66dff4c6542f734f388c990
Husnain <Husnain.tayab@hotmail.com>
2025-10-28 20:10:02 +0500

    While Table

--- while loop table.py
+++ while loop table.py
@@ -1,6 +1,6 @@
-number = int(input("Enter your number : "))
+num = int(input("Enter your number : "))
 
 i = 1
 while(i<11):
-    print(f"{number} x {i} = {number*i}")
+    print(f"{num} x {i} = {num*i}")
     i = i+1
