8b5a0e8a8f2b2a48f31e9a1fd8ccfe765476c203
Husnain <Husnain.tayab@hotmail.com>
2025-10-28 20:14:49 +0500

    List program

--- if-else-list.py
+++ if-else-list.py
@@ -1,10 +1,10 @@
-nums = [1 ,3 ,4,5,6,7]
+nums = [1 ,3 ,4,5,6,7,9]
 print(type(nums))
 unique = set(nums)
 print(type(unique))
 if 8 in nums:
     print("True")
-elif len(unique)==6:
+elif len(unique)==7:
     print("All unique")
 else:
     print("Forward")
