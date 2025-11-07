03dc7d5ac91d3db711db44f690dd53827dbecf74
Husnain <Husnain.tayab@hotmail.com>
2025-11-07 21:45:01 +0500

    Tax Calculator

--- tax calculator.py
+++ tax calculator.py
@@ -3,9 +3,12 @@ income = int(input("Enter your income : "))
 
 if income<= 5000:
     print("NO TAX")
+
 elif income == 5001 or income <= 10000:
     print("10% Tax")
+
 elif income==1001 or income<= 20000:
     print(" 20% Tax")
+    
 elif income>20000:
     print("40% Tax")
