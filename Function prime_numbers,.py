a06c6803537d418c16ef5b3230ce342d0ffd444e
Husnain <Husnain.tayab@hotmail.com>
2025-11-28 21:30:21 +0500

    Function Prime number

---
+++ Function prime_numbers,.py
@@ -0,0 +1,13 @@
+def prime_numbers(n):
+    primes = []
+    for num in range(2, n):
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+
+print(prime_numbers(20))
