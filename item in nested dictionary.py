73ee174a03c6478a6ca5df11d87b79aa09f3f4a2
Husnain <Husnain.tayab@hotmail.com>
2025-12-07 00:27:15 +0500

    item in nested dictionary

---
+++ item in nested dictionary.py
@@ -0,0 +1,11 @@
+
+def find_item(data, target):
+    for key, value in data.items():
+        if isinstance(value, dict):
+            result = find_item(value, target)
+            if result:
+                return [key] + result
+        else:
+            if target in value:
+                return [key]
+    return None
