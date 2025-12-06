c40ef8238e8f1be8fbbadbd2cc59ae095ad5b8fa
Husnain <Husnain.tayab@hotmail.com>
2025-12-07 00:31:42 +0500

    Remove consecutive duplicates

---
+++ Remove consecutive duplicates.py
@@ -0,0 +1,7 @@
+def remove_duplicates(lst):
+    if len(lst) <= 1:
+        return lst
+    rest = remove_duplicates(lst[1:])
+    if lst[0] == rest[0]:
+        return rest
+    return [lst[0]] + rest
