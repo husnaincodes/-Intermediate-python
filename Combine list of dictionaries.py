1089c611df3d66badfb42c41cd6d37c137162f13
Husnain <Husnain.tayab@hotmail.com>
2025-12-08 21:01:24 +0500

    Combine list of dictionaies

---
+++ Combine list of dictionaries.py
@@ -0,0 +1,10 @@
+def combine_dicts(lst, index=0, result=None):
+    if result is None:
+        result = {}
+    if index == len(lst):
+        return result
+    
+    for k in lst[index]:
+        result[k] = result.get(k, 0) + 1
+    
+    return combine_dicts(lst, index + 1, result)
