e2e05250a9507520dcf37497ea9e6f1b44490683
Husnain <Husnain.tayab@hotmail.com>
2025-12-24 23:04:59 +0500

    Area Formula

--- area.py
+++ area.py
@@ -8,4 +8,5 @@ sum_sides =(num1+num2+num3)/2
 area = float(math.sqrt(sum_sides*(sum_sides-num1)*(sum_sides-num2)*(sum_sides-num3)))
 triangle_area = float(area)
 
+
 print(f"The area of Triangle is : {triangle_area}")  
