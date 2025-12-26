3402a855fe5af2136ce457e3efc12a131bf1fdfa
Husnain <Husnain.tayab@hotmail.com>
2025-12-26 22:20:20 +0500

    If-else

--- if-else.py
+++ if-else.py
@@ -1,5 +1,3 @@
-
-
 list = ["Bilal","Sara","Omar","Ayesha","Usman","Mariam","Hassan","Noor","Imran","Iqra","Shahzaib","Maha","Rizwan","Anaya","Husnain"
 ]
 name = input("ENTER YOUR TO CHECK YOUR NAME IN THE LIST : ")
