0361c68a9d54d29eb760ccc8c3c4969eba1b1631
Husnain <Husnain.tayab@hotmail.com>
2026-01-10 20:54:19 +0500

    Convert list to tuple

--- Convert list to tuple (recursively).py
+++ Convert list to tuple (recursively).py
@@ -1,5 +1,6 @@
 def list_to_tuple(lst):
+
     if not lst:
         return ()
     return (lst[0],) + list_to_tuple(lst[1:])
-print(list_to_tuple([1,23,45,667,5]))
+print(list_to_tuple([1,23,45,667,5,10]))
