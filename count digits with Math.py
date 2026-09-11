88feb0dbb8e834ee4e493a53912534e4431d8bb0
husnaincodes <Husnain.tayab@hotmail.com>
2026-09-11 22:16:40 +0500

    Count Digit Program

--- count digits with Math.py
+++ count digits with Math.py
@@ -1,7 +1,11 @@
 from math import log10 
 
 def count_digits(num):
+    if num == 0:
+        return 1
     return int(log10(num)) + 1 
+
 num = int(input("Enter a number: "))
+
 
 print("Number of digits in", num, "is:", count_digits(num))
