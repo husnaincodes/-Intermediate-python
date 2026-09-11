cec132ce32fadac11aac71505ee971f0fe6bce7b
husnaincodes <Husnain.tayab@hotmail.com>
2026-09-11 22:17:59 +0500

    Add Spacing

--- Count nodes in directory tree.py
+++ Count nodes in directory tree.py
@@ -1,6 +1,9 @@
 def count_nodes(tree):
+    
     name, children = tree
-    total = 1
+    total =1
+    
     for child in children:
+        
         total += count_nodes(child)
     return total
